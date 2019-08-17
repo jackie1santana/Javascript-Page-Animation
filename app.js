89154acea766a1e281de575d53f42e1b35363e8e
Jackie Santana <jackiebrown.nyc@gmail.comgit config --global user.email jackiebrown.nyc@gmail.com>
2019-08-16 19:21:45 -0500

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-new hoverEffect({
-    parent: document.querySelector('.distortion'),
-    intensity: 0.2,
-    image1: './bw1.jpg',
-    image2: './bw2.jpg',
-    angle: Math.PI / 4,
-    displacementImage: './diss.png'
-})
+const hero = document.querySelector('.hero')
+const slider = document.querySelector('.slider')
+const logo = document.querySelector('#logo')
+const hamburger = document.querySelector('.hamburger')
+const headline = document.querySelector('.headline')
+
+const tl = new TimelineMax();
+
+tl.fromTo(
+    hero,1, 
+    {height: "0%"}, 
+    {height: '80%', ease: Power2.easeInOut}
+    )
+.fromTo(
+    hero,
+     1.2, {width: '100%'},
+    {width: '80%', ease: Power2.easeInOut}
+    )
+.fromTo(
+    slider, 
+    1.2, {x: "-100%"}, 
+    {x: '0%;', ease: Power2.easeInOut}
+    )
+.fromTo(
+     logo, 0.5, 
+     {opacity: 0, x: 30}, 
+     {opacity: 1, x: 0}, "-=0.5"
+     )
+.fromTo(
+     hamburger, 0.5, 
+     {opacity: 0, x: 30}, 
+     {opacity: 1, x: 0}, "-=0.5"
+     )
+.fromTo(
+     headline, 0.5, 
+     {opacity: 0, x: 30}, 
+     {opacity: 1, x: 0}, "-=0.5"
+     )
